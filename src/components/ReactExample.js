bba1b5edfdce54d2ea3f1b12dc6df5e9ea167f2d
Tyler Royer <troyer@bgsu.edu>
2021-02-05 13:36:07 -0500

    Add examples for writing new documents to the database

--- src/components/ReactExample.js
+++ src/components/ReactExample.js
@@ -10,10 +10,28 @@ export default function ReactExample(props) {
 
   const [quote, setQuote] = useState(0);
 
+  // To add a new student:
+  /*db.collection("students").add ({
+      course: "course_id",
+      student_uid: "student_uid",
+      name: "Test Student",
+      bgsu_id: "0011111111",
+      assignments: "{\"assignments\":[]}"
+    }) */
+
+  // To add a new instructor:
+  /*db.collection("instructors").add ({
+      course: "course_id",
+      instructor_uid: "instructor_uid",
+      name: "Test Instructor
+    }) */
+
+  // Pull info from database:
   db.collection("quotes")
     .doc("JfAQwxQZdhyCBnG2i6PA")
     .get()
     .then((query) => setQuote(query.data().body));
+
   return (
     <div className="App">
       <header className="App-header">
