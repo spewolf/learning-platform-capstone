70d5fe51f1bb5913e6f84a464e2d1454ee7f46e9
Spencer Wolf <spwolf18@gmail.com>
2021-02-27 16:09:19 -0500

    Implement question functionality for displaying results

---
+++ src/stories/Question.stories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import mockAssignment from '../mocks/practiceAssignment.mock.json';
+
+import Question from '../components/Question';
+
+const storyMetaData = {
+  title: 'Question',
+  component: Question,
+};
+export default storyMetaData;
+
+const Template = (args) => <Question {...args} />;
+
+export const PracticeQuestion = Template.bind({});
+PracticeQuestion.args = {
+  question: mockAssignment.questions[0],
+  onChange: (e) => console.log(e.target.value),
+  onCheck: () => console.log("Checking"),
+  onNext: () => console.log("Nexting"),
+};
+
+export const CorrectPracticeQuestion = Template.bind({});
+CorrectPracticeQuestion.args = {
+  question: mockAssignment.questions[0],
+  onChange: (e) => console.log(e.target.value),
+  onCheck: () => console.log("Checking"),
+  onNext: () => console.log("Nexting"),
+  result: true,
+};
+
+export const IncorrectPracticeQuestion = Template.bind({});
+IncorrectPracticeQuestion.args = {
+  question: mockAssignment.questions[0],
+  onChange: (e) => console.log(e.target.value),
+  onCheck: () => console.log("Checking"),
+  onNext: () => console.log("Nexting"),
+  result: false,
+};
