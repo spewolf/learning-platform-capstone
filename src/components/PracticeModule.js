ddb48f731342a21e682de08918ff5f50b9602038
Tyler Royer <troyer@bgsu.edu>
2021-03-01 11:31:21 -0500

    Stitch practice module together

--- src/components/PracticeModule.js
+++ src/components/PracticeModule.js
@@ -10,13 +10,26 @@ import {
   generateSubtractionQuestion,
   generateAssignment
 } from '../helpers/AssignmentGenerator'
+import Quiz from './Quiz'
 
 export default function PracticeModule(props) {
+  const emptyAssignment = {
+    "uid": "",
+    "type": "",
+    "due": "",
+    "course": "",
+    "title": "",
+    "questions": [
+    ]
+  }
+
   const [binToDec, setBinToDec] = React.useState(false)
   const [decToBin, setDecToBin] = React.useState(false)
   const [add, setAdd] = React.useState(false)
   const [sub, setSub] = React.useState(false)
   const [numberOfQuestions, setNumberOfQuestions] = React.useState("")
+  const [assignment, setAssignment] = React.useState(emptyAssignment)
+  const [showAssignment, setShowAssignment] = React.useState(false)
 
   // Called when the user clicks the "begin" button.
   const begin = (event) => {
@@ -37,8 +50,8 @@ export default function PracticeModule(props) {
       functions.push(generateSubtractionQuestion)
     }
 
-    // TODO: Must send info to Spencer's component at this point.
-    console.log(generateAssignment(functions, parseInt(numberOfQuestions), "ungraded", "dummyCourse", "dummyTitle"))
+    setAssignment(generateAssignment(functions, parseInt(numberOfQuestions), "ungraded", "dummyCourse", "dummyTitle"));
+    setShowAssignment(true);
   };
 
   // Checkbox onChange listeners
@@ -62,55 +75,64 @@ export default function PracticeModule(props) {
 
   return (
     <div>
-      <Container style={{backgroundColor: '#ffffff'}}>
-        <form onSubmit={begin}>
-          <h1>Practice</h1>
-          <div style={{display: "flex"}}>
-            <div style={{width: "50%", margin: ".3em"}}>
-              <h4>From Assignments</h4>
-              <Paper elevation={3}>
-                {/* TODO: This should pull from the db to see whether or not the user has any assignments. */}
-                <p>You have no practice assignments.</p>
-              </Paper>
+      <div style={showAssignment ? {display: "none"} : {}}>
+        <Container>
+          <form onSubmit={begin}>
+            <h1>Practice</h1>
+            <div style={{display: "flex"}}>
+              <div style={{width: "50%", margin: ".3em"}}>
+                <h4>From Assignments</h4>
+                <Paper elevation={3}>
+                  {/* TODO: This should pull from the db to see whether or not the user has any assignments. */}
+                  <p>You have no practice assignments.</p>
+                </Paper>
+              </div>
+              <div style={{width: "50%", margin: ".3em"}}>
+                <h4>Custom</h4>
+                <Paper elevation={3}>
+                  <div style={{display: "flex"}}>
+                    <Checkbox name="BinToDec" onChange={onBinToDecChanged}/>
+                    <p>Binary to Decimal Conversion</p>
+                  </div>
+                  <div style={{display: "flex"}}>
+                    <Checkbox name="DecToBin" onChange={onDecToBinChanged}/>
+                    <p>Decimal to Binary Conversion</p>
+                  </div>
+                  <div style={{display: "flex"}}>
+                    <Checkbox name="Add" onChange={onAddChanged}/>
+                    <p>Binary Addition</p>
+                  </div>
+                  <div style={{display: "flex"}}>
+                    <Checkbox name="Sub" onChange={onSubChanged}/>
+                    <p>Binary Subtraction</p>
+                  </div>
+                  <div style={{display: "flex"}}>
+                    <p>How many problems?</p>
+                    <Input style={{margin: "1em"}} onChange={onInputChanged}/>
+                  </div>
+                </Paper>
+              </div>
             </div>
-            <div style={{width: "50%", margin: ".3em"}}>
-              <h4>Custom</h4>
-              <Paper elevation={3}>
-                <div style={{display: "flex"}}>
-                <Checkbox name="BinToDec" onChange={onBinToDecChanged}/><p>Binary to Decimal Conversion</p>
-                </div>
-                <div style={{display: "flex"}}>
-                  <Checkbox name="DecToBin" onChange={onDecToBinChanged}/><p>Decimal to Binary Conversion</p>
-                </div>
-                <div style={{display: "flex"}}>
-                  <Checkbox name="Add" onChange={onAddChanged}/><p>Binary Addition</p>
-                </div>
-                <div style={{display: "flex"}}>
-                  <Checkbox name="Sub" onChange={onSubChanged}/><p>Binary Subtraction</p>
-                </div>
-                <div style={{display: "flex"}}>
-                  <p>How many problems?</p><Input style={{margin: "1em"}} onChange={onInputChanged}/>
-                </div>
-              </Paper>
-            </div>
-          </div>
-          <Button
-            disabled={
-              // Disable the button if none of the checkboxes are checked or if the input is NaN.
-              !(binToDec || decToBin || add || sub)
-              || isNaN(parseInt(numberOfQuestions))
-              || numberOfQuestions.length === 0
-            }
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-          >
-            Begin
-          </Button>
-        </form>
+            <Button
+              disabled={
+                // Disable the button if none of the checkboxes are checked or if the input is NaN.
+                !(binToDec || decToBin || add || sub)
+                || isNaN(parseInt(numberOfQuestions))
+                || numberOfQuestions.length === 0
+              }
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Begin
+            </Button>
+          </form>
+        </Container>
+      </div>
+      <Container style={showAssignment ? {} : {display: "none"}}>
+        <Quiz assignment={assignment}/>
       </Container>
     </div>
   )
 }
-
