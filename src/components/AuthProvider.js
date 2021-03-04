831df718342ee5c7bacc67c4350291237943b17b
David Ash <dash@bgsu.edu>
2021-03-03 21:44:06 -0500

    Changed loading screen

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import firebase from "firebase";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export const AuthContext = React.createContext();
 
@@ -28,7 +29,7 @@ export const AuthProvider = ({ children }) => {
   }, [app, db]);
 
   if (pending) {
-    return <>Loading...</>;
+    return <CircularProgress size="7em" style={{position: "fixed", top: `calc(50% - 3.5em)`, left: `calc(50% - 3.5em)`}}/>;
   }
 
   return (
