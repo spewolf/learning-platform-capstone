c0ccb5bf5be3bae3e11c2530f781964f81fb4b7b
David Ash <dash@bgsu.edu>
2021-03-24 16:22:44 -0400

    Add new assignment UI in Instructor Dashboard

--- src/components/InstructorDashboard.js
+++ src/components/InstructorDashboard.js
@@ -1,36 +1,159 @@
 import React from 'react';
-import { Container, Paper, Typography, Accordion, AccordionSummary, AccordionDetails, List, ListItem, ListItemText, Link } from '@material-ui/core';
+import { Container, Paper, Typography, Accordion, AccordionSummary, AccordionDetails, IconButton, FormLabel, List, ListItem, ListItemText, Link, Button, Grid, Backdrop, TextField } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CreateIcon from "@material-ui/icons/Create";
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      width: '100%',
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-      flexGrow: 1,
-      overflow: 'auto',
-    },
-    container: {
-      paddingTop: theme.spacing(4),
-      paddingBottom: theme.spacing(4),
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
-  }));
+  root: {
+    width: '100%',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    zIndex: 1,
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+  newAssignmentQuestions: {
+    alignItems: "center", 
+    display: "flex", 
+    justify: "space-around",
+  }
+}));
 
-  export default function InstructorDashboard(props) {
-    const classes = useStyles();
-    return (
+// let docQuery = this._db.collection('communityProgramPost');
+// const response = await docQuery.get();
+// const post = response.docs.map(doc => {
+//   const post = doc.data();
+//   post.id = doc.id;
+//   return post;
+// });
+
+export default function InstructorDashboard(props) {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [submittable, setSubmit] = React.useState(false);
+  const [numBinToDec, setBinToDec] = React.useState("");
+  const [numDecToBin, setDecToBin] = React.useState("");
+  const [numBinAdd, setBinAdd] = React.useState("");
+  const [numBinSub, setBinSub] = React.useState("");
+  const handleToggle = (e) => {
+    setOpen(!open);
+  };
+  const handleSubmittable = () => {
+    if (isNaN(parseInt(numBinToDec)) || 
+    isNaN(parseInt(numDecToBin)) || 
+    isNaN(parseInt(numBinAdd)) || 
+    isNaN(parseInt(numBinSub)) || 
+    (parseInt(numBinToDec) === 0 && 
+    parseInt(numDecToBin) === 0 && 
+    parseInt(numBinAdd) === 0 && 
+    parseInt(numBinSub) === 0) ||
+    parseInt(numBinToDec) < 0 ||
+    parseInt(numDecToBin) < 0 ||
+    parseInt(numBinAdd) < 0 ||
+    parseInt(numBinSub) < 0) setSubmit(false);
+    else setSubmit(true);
+  };
+  const handleSubmit = (e) => {
+    // TODO: pass quiz object up to database
+  };
+  const handleBinToDec = (e) => {
+    setBinToDec(e.target.value);
+    handleSubmittable();
+  };
+  const handleDecToBin = (e) => {
+    setDecToBin(e.target.value);
+    handleSubmittable();
+  };
+  const handleBinAdd = (e) => {
+    setBinAdd(e.target.value);
+    handleSubmittable();
+  };
+  const handleBinSub = (e) => {
+    setBinSub(e.target.value);
+    handleSubmittable();
+  };
+
+  return (
+    <div>
+      <Backdrop className={classes.backdrop} open={open}>
+        <form>
+          <Paper style={{padding: "2em", paddingTop: "0.5em", justify: "center"}}>
+            <Grid container alignItems="center" justify="space-between">
+              <h1>New Assignment</h1>
+              <IconButton onClick={handleToggle}>
+                <CloseIcon />
+              </IconButton>
+            </Grid>
+            <div className={classes.newAssignmentQuestions}>
+              <div style={{width: "40%"}}>
+                <FormLabel label="Binary to Decimal:" value="BinToDec">Binary to Decimal:</FormLabel>
+              </div>
+              <div style={{width: "1em"}} />
+              <TextField onChange={handleBinToDec} id="filled-basic" defaultValue="0" label="Number" variant="filled" />
+            </div>
+            <div className={classes.newAssignmentQuestions}>
+              <div style={{width: "40%"}}>
+                <FormLabel label="Decimal to Binary:" value="DecToBin">Decimal to Binary:</FormLabel>
+              </div>
+              <div style={{width: "1em"}} />
+              <TextField onChange={handleDecToBin} id="filled-basic" defaultValue="0" label="Number" variant="filled" />
+            </div>
+            <div className={classes.newAssignmentQuestions}>
+              <div style={{width: "40%"}}>
+                <FormLabel label="Binary Addition:" value="BinAdd">Binary Addition:</FormLabel>
+              </div>
+              <div style={{width: "1em"}} />
+              <TextField onChange={handleBinAdd} id="filled-basic" defaultValue="0" label="Number" variant="filled" />
+            </div>
+            <div className={classes.newAssignmentQuestions}>
+              <div style={{width: "40%"}}>
+                <FormLabel label="Binary Subtraction" value="BinSub">Binary Subtraction:</FormLabel>
+              </div>
+              <div style={{width: "1em"}} />
+              <TextField onChange={handleBinSub} id="filled-basic" defaultValue="0" label="Number" variant="filled" />
+            </div>
+            <Button onClick={handleSubmit} variant="contained" type="submit" disabled={!submittable}>Submit</Button>
+          </Paper>
+        </form>
+      </Backdrop>
       <Container component="main" className={classes.content} maxWidth="" style={{display: "flex"}}>
         <div style={{display: "flex", width: "100%", margin: "1em"}}>
           <div style={{width: "3.5%"}} />
-          <div style={{width: "43%"}}>
-            <Paper elevation={3}>
-              <h1 style={{paddingLeft: "0.9em", paddingTop: "0.6em"}}>Ongoing</h1>
+          <div style={{width: "43%", display: "stretch"}}>
+            <Paper elevation={3} style={{marginTop: "1.35em"}}>
+              <Grid 
+                container 
+                alignItems="baseline" 
+                style={{paddingRight: "1.2em", alignItems: "center"}} 
+                direction="flex-end" 
+                justify="space-between"
+              >
+                <h1 style={{paddingLeft: "0.9em"}}>Ongoing</h1>
+                <Button 
+                  startIcon={<CreateIcon />} 
+                  variant="contained" 
+                  color="primary"
+                  onClick={handleToggle}
+                >
+                  Create
+                </Button>
+              </Grid>
+              {/* TODO: pull all ongoing assignments from database, map to an accordion */}
               <Accordion style={{paddingLeft: "1em"}}>
                 <AccordionSummary
                   expandIcon={<ExpandMoreIcon />}
@@ -100,5 +223,6 @@ const useStyles = makeStyles((theme) => ({
           </div>
         </div>
       </Container>
-    );
-  }
+    </div>
+  );
+}
