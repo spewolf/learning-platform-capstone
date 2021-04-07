faeabb652f122facaffb5066d954b95401d49896
David Ash <dash@bgsu.edu>
2021-04-07 13:43:36 -0400

    Validation of input completed

--- src/components/InstructorDashboard.js
+++ src/components/InstructorDashboard.js
@@ -1,9 +1,7 @@
 import React, { useContext, useEffect } from 'react';
-import { Container, Paper, Typography, Accordion, AccordionSummary, 
-         AccordionDetails, IconButton, FormLabel, List, ListItem, 
-         ListItemText, Link, Button, Grid, Backdrop, TextField } from '@material-ui/core';
+import { Container, Paper, IconButton, FormLabel, List, ListItem, 
+         ListItemText, Button, Grid, Backdrop, TextField } from '@material-ui/core';
 import { makeStyles } from "@material-ui/core/styles";
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import CreateIcon from "@material-ui/icons/Create";
 import CloseIcon from "@material-ui/icons/Close";
 import { getAssignmentsForCourse } from "../helpers/DatabaseHelper";
@@ -22,7 +20,7 @@ import {
   generateAdditionQuestion,
   generateSubtractionQuestion,
   generateGradedAssignment
-} from '../helpers/AssignmentGenerator'
+} from '../helpers/AssignmentGenerator';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -58,6 +56,7 @@ export default function InstructorDashboard(props) {
   const [open, setOpen] = React.useState(false);
   const [assignmentName, setAssignmentName] = React.useState("");
   const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [dateErr, setDateErr] = React.useState(true);
   const [ongoingAssignments, setOngoing] = React.useState([]);
   const [completedAssignments, setCompleted] = React.useState([]);
   const [numBinToDec, setBinToDec] = React.useState("");
@@ -72,6 +71,15 @@ export default function InstructorDashboard(props) {
   const userCourse = currentUser.data.course;
   const app = firebase.apps[0];
   const db = firebase.firestore(app);
+  const BTDQErr = ((parseInt(numBinToDec) === 0 || numBinToDec === "") && parseInt(numBTDPts) > 0);
+  const BTDPErr = (parseInt(numBinToDec) > 0 && (parseInt(numBTDPts) === 0 || numBTDPts === ""));
+  const DTBQErr = ((parseInt(numDecToBin) === 0 || numDecToBin === "") && parseInt(numDTBPts) > 0);
+  const DTBPErr = (parseInt(numDecToBin) > 0 && (parseInt(numDTBPts) === 0 || numDTBPts === ""));
+  const BAQErr = ((parseInt(numBinAdd) === 0 || numBinAdd === "") && parseInt(numBAPts) > 0);
+  const BAPErr = (parseInt(numBinAdd) > 0 && (parseInt(numBAPts) === 0 || numBAPts === ""));
+  const BSQErr = ((parseInt(numBinSub) === 0 || numBinSub === "") && parseInt(numBSPts) > 0);
+  const BSPErr = (parseInt(numBinSub) > 0 && (parseInt(numBSPts) === 0 || numBSPts === ""));
+  const titleErr = assignmentName === "";
 
   // TODO: Relegate this to respective error functions
   const submittable = !((parseInt(numBinToDec) === 0 && parseInt(numDecToBin) === 0 && 
@@ -82,19 +90,20 @@ export default function InstructorDashboard(props) {
                         numBinAdd === "" && numBinSub === "") ||
                         (numBTDPts === "" && numDTBPts === "" && 
                         numBAPts === "" && numBSPts === "") ||
-                        assignmentName === "" || selectedDate < Date.now());
-
+                        dateErr || BTDQErr || BTDPErr || DTBQErr || DTBPErr || 
+                        BAQErr || BAPErr || BSQErr || BSPErr || titleErr);
+  
   const handleNumInput = (e, setter) => {
     let flag = true;
     
     for (let i = 0; i < e.target.value.length && flag; i++)
       if (isNaN(e.target.value[i]) || e.target.value[i] === " ") flag = false; 
     if (flag) setter(e.target.value);
-    // if (e.target.value === "") setter("0");
-    // else setter(e.target.value);
   };
   const handleDateChange = (date) => {
     setSelectedDate(date);
+    if (date < Date.now()) setDateErr(true);
+    else setDateErr(false);
   };
   const handleToggle = () => {
     setOpen(!open);
@@ -102,7 +111,7 @@ export default function InstructorDashboard(props) {
   const handleName = (e) => {
     setAssignmentName(e.target.value);
   };
-  const handleSubmit = (e) => {
+  const handleSubmit = () => {
     let generatorMap = new Map();
     generatorMap.set(generateBinToDecQuestion, {"quantity": numBinToDec, "points": parseInt(numBTDPts)});
     generatorMap.set(generateDecToBinQuestion, {"quantity": numDecToBin, "points": parseInt(numDTBPts)});
@@ -112,6 +121,19 @@ export default function InstructorDashboard(props) {
     db.collection("assignments").add(newAssignment);
     // console.log(newAssignment);
     // TODO: Refresh assignments
+    handleToggle();
+  };
+  const handleCancel = () => {
+    setAssignmentName("");
+    setBinToDec("");
+    setDecToBin("");
+    setBinAdd("");
+    setBinSub("");
+    setBTDPts("");
+    setDTBPts("");
+    setBAPts("");
+    setBSPts("");
+    setSelectedDate(Date.now());
     handleToggle();
   };
 
@@ -135,9 +157,18 @@ export default function InstructorDashboard(props) {
     <div>
       <Backdrop className={classes.backdrop} open={open}>
         <form>
-          <Paper style={{padding: "1.5em", paddingBottom: "2em", justify: "center", width:"82%"}}>
+          <Paper style={{padding: "1.5em", paddingBottom: "1.5em", justify: "center", width:"82%"}}>
             <Grid container alignItems="flex-start" justify="space-around">
-              <TextField style={{paddingBottom: "1em", width: "86.5%"}} onChange={handleName} placeholder="New Assignment" defaultValue="" variant="outlined"/>
+              <TextField 
+                style={{paddingBottom: "1em", width: "86.5%"}} 
+                onChange={handleName} 
+                placeholder="New Assignment" 
+                defaultValue="" 
+                variant="outlined"
+                value={assignmentName}
+                error={titleErr}
+              />
+              
               <IconButton onClick={handleToggle}>
                 <CloseIcon />
               </IconButton>
@@ -151,6 +182,7 @@ export default function InstructorDashboard(props) {
                   onChange={handleDateChange}
                   style={{width: "30%"}}
                   format="MM/dd/yyyy"
+                  error={dateErr}
                 />
                 <KeyboardTimePicker
                   variant="outlined"
@@ -158,6 +190,7 @@ export default function InstructorDashboard(props) {
                   value={selectedDate}
                   onChange={handleDateChange}
                   style={{width: "30%"}}
+                  error={dateErr}
                 />
               </Grid>
             </MuiPickersUtilsProvider>
@@ -166,33 +199,101 @@ export default function InstructorDashboard(props) {
               <div style={{width: "40%"}}>
                 <FormLabel label="Binary to Decimal:" value={numBinToDec}>Binary to Decimal:</FormLabel>
               </div>
-              <TextField onChange={(e) => handleNumInput(e, setBinToDec)} value={numBinToDec} autoComplete="off" style={{maxWidth: "50%"}} id="outlined-basic" placeholder="# Questions" variant="outlined" />
-              <TextField onChange={(e) => handleNumInput(e, setBTDPts)} value={numBTDPts} autoComplete="off" style={{maxWidth: "12%"}} placeholder="Pts" variant="outlined" />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setBinToDec)} 
+                value={numBinToDec} 
+                autoComplete="off" 
+                style={{maxWidth: "50%"}} 
+                id="outlined-basic" 
+                placeholder="# Questions" 
+                variant="outlined" 
+                error={BTDQErr}
+              />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setBTDPts)} 
+                value={numBTDPts} 
+                autoComplete="off" 
+                style={{maxWidth: "12%"}} 
+                placeholder="Pts" 
+                variant="outlined" 
+                error={BTDPErr}
+              />
             </div>
             <div className={classes.newAssignmentQuestions}>
               <div style={{width: "40%"}}>
                 <FormLabel label="Decimal to Binary:" value={numDecToBin}>Decimal to Binary:</FormLabel>
               </div>
-              <TextField onChange={(e) => handleNumInput(e, setDecToBin)} value={numDecToBin} autoComplete="off" style={{maxWidth: "50%"}} id="outlined-basic" placeholder="# Questions" variant="outlined" />
-              <TextField onChange={(e) => handleNumInput(e, setDTBPts)} value={numDTBPts} autoComplete="off" style={{maxWidth: "12%"}} placeholder="Pts" variant="outlined" />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setDecToBin)} 
+                value={numDecToBin} 
+                autoComplete="off" 
+                style={{maxWidth: "50%"}} 
+                id="outlined-basic" 
+                placeholder="# Questions" 
+                variant="outlined" 
+                error={DTBQErr}
+              />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setDTBPts)} 
+                value={numDTBPts} 
+                autoComplete="off" 
+                style={{maxWidth: "12%"}} 
+                placeholder="Pts" 
+                variant="outlined"
+                error={DTBPErr} 
+              />
             </div>
             <div className={classes.newAssignmentQuestions}>
               <div style={{width: "40%"}}>
                 <FormLabel label="Binary Addition:" value={numBinAdd}>Binary Addition:</FormLabel>
               </div>
-              <TextField onChange={(e) => handleNumInput(e, setBinAdd)} value={numBinAdd} autoComplete="off" style={{maxWidth: "50%"}} id="outlined-basic" placeholder="# Questions" variant="outlined" />
-              <TextField onChange={(e) => handleNumInput(e, setBAPts)} value={numBAPts} autoComplete="off" style={{maxWidth: "12%"}} placeholder="Pts" variant="outlined" />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setBinAdd)} 
+                value={numBinAdd} 
+                autoComplete="off" 
+                style={{maxWidth: "50%"}} 
+                id="outlined-basic" 
+                placeholder="# Questions" 
+                variant="outlined" 
+                error={BAQErr}
+              />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setBAPts)} 
+                value={numBAPts} 
+                autoComplete="off" 
+                style={{maxWidth: "12%"}} 
+                placeholder="Pts" 
+                variant="outlined" 
+                error={BAPErr}
+              />
             </div>
             <div className={classes.newAssignmentQuestions}>
               <div style={{width: "40%"}}>
                 <FormLabel label="Binary Subtraction" value={numBinSub}>Binary Subtraction:</FormLabel>
               </div>
-              <TextField onChange={(e) => handleNumInput(e, setBinSub)} value={numBinSub} autoComplete="off" style={{maxWidth: "50%"}} id="outlined-basic" placeholder="# Questions" variant="outlined" />
-              <TextField onChange={(e) => handleNumInput(e, setBSPts)} value={numBSPts} autoComplete="off" style={{maxWidth: "12%"}} placeholder="Pts" variant="outlined" />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setBinSub)} 
+                value={numBinSub} 
+                autoComplete="off" 
+                style={{maxWidth: "50%"}} 
+                id="outlined-basic" 
+                placeholder="# Questions" 
+                variant="outlined" 
+                error={BSQErr}
+                />
+              <TextField 
+                onChange={(e) => handleNumInput(e, setBSPts)} 
+                value={numBSPts} 
+                autoComplete="off" 
+                style={{maxWidth: "12%"}} 
+                placeholder="Pts" 
+                variant="outlined" 
+                error={BSPErr}
+                />
             </div>
             <div style={{padding: "0.5em"}} />
             <Grid container alignItems="flex-start" justify="space-between">
-              <Button onClick={handleToggle} variant="contained">Cancel</Button>
+              <Button onClick={handleCancel} variant="contained">Cancel</Button>
               <Button onClick={handleSubmit} style={{marginRight:"0.4em"}} variant="contained" color="primary" disabled={!submittable}>Submit</Button>
             </Grid>
           </Paper>
@@ -220,51 +321,26 @@ export default function InstructorDashboard(props) {
                   Create
                 </Button>
               </Grid>
-              {ongoingAssignments.map((a) =>
-                <Accordion style={{paddingLeft: "1em", width: "100%"}}>
-                  <AccordionSummary 
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography className={classes.header}>{a.title}</Typography>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    <List>
-                      <ListItem style={{paddingLeft: "2em"}}>
-                        <ListItemText primary="Mean: ">
-                          {/* Implement mean */}
-                        </ListItemText>
-                      </ListItem>
-                      <ListItem style={{paddingLeft: "2em"}}>
-                        <ListItemText primary="Median: ">
-                          {/* Implement median */}
-                        </ListItemText>
-                      </ListItem>
-                      <ListItem>
-                        <Link href="/stats" button>More...</Link>
-                      </ListItem>
-                    </List>
-                  </AccordionDetails>
-                </Accordion>
-              )}
+              <List className={classes.root}>
+                {ongoingAssignments.map((a) => 
+                  <ListItem button component="a" href={`/stats?assignment=${a.id}`}>
+                    <ListItemText style={{paddingLeft: "1em"}} primary={a.title} />
+                  </ListItem>
+                )}
+              </List>
             </Paper>
           </div>
           <div style={{width: "4%"}} />
           <div style={{width: "43%"}}>
             <Paper elevation={3}>
               <h1 style={{paddingLeft: "0.9em", paddingTop: "0.6em"}}>Completed</h1>
-              {completedAssignments.map((a, index) => (
-                <Accordion style={{paddingLeft: "1em", width: "100%"}}>
-                  <AccordionSummary 
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography className={classes.header}>{a.title}</Typography>
-                  </AccordionSummary>
-                </Accordion>
-              ))}
+              <List className={classes.root}>
+                {completedAssignments.map((a) => 
+                  <ListItem button component="a" href={`/stats?assignment=${a.id}`}>
+                    <ListItemText style={{paddingLeft: "1em"}} primary={a.title} />
+                  </ListItem>
+                )}
+              </List>
             </Paper>
           </div>
         </div>
